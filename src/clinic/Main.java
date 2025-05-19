8095bc6ad308e4f3c18223858d2eeb8cb0c03c21
Uluk <slom.uno.60@gmail.com>
2025-05-19 18:12:47 +0600

    add role-based menu switching in Main class

--- src/clinic/Main.java
+++ src/clinic/Main.java
@@ -19,5 +19,28 @@ public class Main {
         String login = user[2];
 
         System.out.println("✅ Welcome, " + name + " (" + role + ")");
+
+        switch (role) {
+            case "doctor" -> showDoctorMenu(name);
+            case "nurse" -> showMedAssistantMenu(name);
+            case "chief" -> showMainDoctorMenu(name);
+            case "patient" -> showPatientMenu(login);
+            default -> System.out.println("⚠ Unknown role.");
+        }
+    }
+    public static void showDoctorMenu(String name) {
+        System.out.println("Doctor menu for " + name);
+    }
+
+    public static void showMedAssistantMenu(String name) {
+        System.out.println("Nurse menu for " + name);
+    }
+
+    public static void showMainDoctorMenu(String name) {
+        System.out.println("Chief doctor menu for " + name);
+    }
+
+    public static void showPatientMenu(String login) {
+        System.out.println("Patient menu for login: " + login);
     }
 }
