8871bafd0a9032428808ee63c308857a0d7d2add
Uluk <slom.uno.60@gmail.com>
2025-05-19 16:22:19 +0600

    add patients, medical_history, and tasks tables

--- src/clinic/DatabaseInitializer.java
+++ src/clinic/DatabaseInitializer.java
@@ -20,6 +20,44 @@ role TEXT NOT NULL,
 name TEXT NOT NULL
 );
 """;
+            // Таблица пациентов
+            String createPatientsTable = """
+CREATE TABLE IF NOT EXISTS patients (
+login TEXT PRIMARY KEY,
+fio TEXT,
+birth_date TEXT,
+height INTEGER,
+weight INTEGER,
+blood_type TEXT,
+treatment_days INTEGER
+);
+""";
+
+// История болезней
+            String createHistory = """
+CREATE TABLE IF NOT EXISTS medical_history (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+patient_login TEXT,
+date TEXT,
+diagnosis TEXT,
+FOREIGN KEY (patient_login) REFERENCES users(login)
+);
+""";
+
+// Поручения медсестрам
+            String createTasksTable = """
+CREATE TABLE IF NOT EXISTS tasks (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+description TEXT,
+doctor_login TEXT,
+nurse_login TEXT,
+status TEXT,
+date_assigned TEXT,
+date_done TEXT,
+UNIQUE(description, doctor_login, nurse_login, date_assigned)
+);
+""";
+
             stmt.execute(createUsers);
 
         } catch (Exception e) {
